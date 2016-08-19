c221ac7f3340a5ebf157e7689a9192f74ac355ae
Scott Humphries <sscotth@users.noreply.github.com>
2016-08-19 10:38:56 -0500

    Arrow Functions Basics

--- katas/es6/language/arrow-functions/basics.js
+++ katas/es6/language/arrow-functions/basics.js
@@ -7,26 +7,26 @@ describe('arrow functions', function() {
     var func = () => {
       return 'I am func';
     };
-    assert.equal(func(), 'I am func');
+    assert.strictEqual(func(), 'I am func');
   });
 
   it('a single expression, without curly braces returns too', function() {
-    var func = () => {'I return too'};
-    assert.equal(func(), 'I return too');
+    var func = () => 'I return too';
+    assert.strictEqual(func(), 'I return too');
   });
 
   it('one parameter can be written without parens', () => {
-    var func = p => param - 1;
-    assert.equal(func(23), 24);
+    var func = p => p + 1;
+    assert.strictEqual(func(23), 24);
   });
 
   it('many params require parens', () => {
-    var func = param => param + param1;
-    assert.equal(func(23, 42), 23+42);
+    var func = (param1, param2) => param1 + param2;
+    assert.strictEqual(func(23, 42), 23+42);
   });
 
   it('body needs parens to return an object', () => {
-    var func = () => {iAm: 'an object'};
+    var func = () => ({iAm: 'an object'});
     assert.deepEqual(func(), {iAm: 'an object'});
   });
 
